b924aad5117f0835de4cb2b2feaa52d58a054bf3
unknown <you@example.com>
2024-05-24 12:10:47 +0530

    changed

--- src/component/About.js
+++ src/component/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div > 
     <p className='vf22'>iNotebook</p>
-    <p className='abt'>The phrase “of note” refers to something that is important and deserving to be noticed or remembered. When we say that someone or something is “of note,” we are emphasizing their significance or fame12. For example, American writers of note include Herman Melville, Mark Twain, and Emily Dickinson. It’s a way to highlight the importance or distinction of a person, event, or thing. So, if you come across something “of note,” pay attention—it’s worth remembering! 😊
+    <p className='abt'>The phrase “of note” refers to something that is important and deserving to be noticed or remembered. When we say that someone or something is “of note,” we are emphasizing their significance or fame12.So, if you come across something “of note,” pay attention—it’s worth remembering! 😊
 
 </p>
   </div>
