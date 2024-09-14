86033c8b6b60bbdb2bfa0e8e75bd7fd5742e2a6b
gopilagah15 <157889090+gopilagah15@users.noreply.github.com>
2024-09-14 17:24:29 +0530

    donedone

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
           <Route exact path='/home' element={<Home showAlert={showAlert} />} /> 
           <Route exact path='/about' element={<About showAlert={showAlert} />} /> 
           <Route exact path='/signup' element={<Signup showAlert={showAlert} />} /> 
-          <Route exact path='/login' element={<Login showAlert={showAlert} />} /> 
+          <Route exact path='/' element={<Login showAlert={showAlert} />} /> 
           </Routes>
           </section>
     </BrowserRouter> 
