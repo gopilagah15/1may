5cdec236cfbc9cc698feda73d5f8a3ea6cf81371
gopilagah15 <157889090+gopilagah15@users.noreply.github.com>
2024-09-16 08:59:43 +0530

    finalk

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -44,19 +44,19 @@ const Signup = (props) => {
     <div className='foo'>
         <form action="" onSubmit={handleSubmit}> 
   <div className="mb-3">
-    <label htmlFor="name" className="form-label">Name</label>
-    <input type="text" className="form-control" id="name" name='name' onChange={onChange} value={name} aria-describedby="emailHelp"/>
+    <label htmlFor="name" className="form-label xcxx">Name</label>
+    <input type="text" className="form-control xcxx" id="name" name='name' onChange={onChange} value={name} aria-describedby="emailHelp"/>
      </div>
   <div className="mb-3">
-    <label htmlFor="email" className="form-label">Email</label>
-    <input type="text" className="form-control" id="email" name='email' onChange={onChange} value={email} aria-describedby="emailHelp"/>
+    <label htmlFor="email" className="form-label xcxx">Email</label>
+    <input type="text" className="form-control xcxx" id="email" name='email' onChange={onChange} value={email} aria-describedby="emailHelp"/>
      </div>
    <div className="mb-3">
-    <label htmlFor="password" className="form-label">Password</label>
-    <input type="text" className="form-control" id="password" name='password' onChange={onChange} value={password}  />
+    <label htmlFor="password" className="form-label xcxx">Password</label>
+    <input type="text" className="form-control xcxx" id="password" name='password' onChange={onChange} value={password}  />
   </div> 
   
-  <button   type="submit" className="btn btn-primary"  >Sign up</button>
+  <button   type="submit" className="btn btn-primary xcxx"  >Sign up</button>
 </form> 
 
     </div>
@@ -65,7 +65,3 @@ const Signup = (props) => {
 }
 
 export default Signup
-
-
-
-
